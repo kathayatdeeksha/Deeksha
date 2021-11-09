ce9a40c4e89348eac6fba57cc3f06b395539015a
kathayatdeeksha <93529260+kathayatdeeksha@users.noreply.github.com>
2021-11-09 18:56:42 +0530

    spring bean factory

---
+++ SpringBeanFactory/src/com/demo/Message.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+
+
+public class Message {
+
+//this is my property
+private String messge;
+
+
+
+public String getMessge() {
+return messge;
+}
+
+
+
+public void setMessge(String messge) {
+this.messge = messge;
+}
+
+
+
+}
+
+
---
+++ SpringBeanFactory1/src/com/demo/Message.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+public class Message {
+
+	//this is my property
+	private String message;
+
+
+
+	public String getMessage() {
+	return message;
+	}
+
+
+
+	public void setMessage(String message) {
+	this.message = message;
+	}
+
+
+
+	}
